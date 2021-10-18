a218e72dcf2df0db7cf9ae336fe20a9312bf3cfb
Mulgh <moktanmulgh@gmail.com>
2021-10-18 11:36:20 +0545

    Initial commit

---
+++ day4_2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+X=np.arange(25)
+Y=np.random.randn(25)
+x1=0
+y1=0
+fig=plt.figure()
+for i in range(0,10):
+	#plt.ion()
+	plt.clf()
+	x1=x1+0.2
+	y1=y1-0.2
+	plt.scatter(X, Y, color='blue')
+	plt.scatter(x1,y1, color='r')
+	plt.pause(0.3)
+	
+plt.show()
+
+	
